036659224b22fc6313d5abeebafd2d8cf1cd910f
abhinavyarragunta <148981364+abhinavyarragunta@users.noreply.github.com>
2023-10-25 10:24:40 -0400

    Added decoder method

--- main.py
+++ main.py
@@ -8,5 +8,16 @@ def encoder(password):
         a = a+b
     return a
 
+def decoder(password):
+    password = list(password)
+    for i in range(len(password)):
+        password[i] = int(password[i])
+        password[i] -= 3
+        password[i] = str(password[i])
+    password = ''.join(password)
+    return password
+        
+
 if __name__ == '__main__':
     print(encoder("12345555"))
+    print(decoder(encoder('12345555')
