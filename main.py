52bbfe50ac0258edc40c7f910a1df0c466d13352
Richnguyen7 <richnguyen423@gmail.com>
2023-10-31 12:45:21 -0400

    hi

--- main.py
+++ main.py
@@ -12,12 +12,32 @@ def decoder(password):
     password = list(password)
     for i in range(len(password)):
         password[i] = int(password[i])
-        password[i] -= 3
+        password[i] += 7
+        password[i] %= 10
         password[i] = str(password[i])
     password = ''.join(password)
     return password
         
 
+
+def main():
+    global password, encoded_password
+    quit_program = False
+    while not quit_program:
+        menu = 'Menu'
+        print(menu)
+        print('-' * len(menu))
+        print('1. Encode\n2. Decode\n3. Quit')
+        menu_option = input('Please enter an option: ')
+        if menu_option == '1':
+            password = input('Please enter your password to encode: ')
+            encoded_password = encode(password)
+            print('Your password has been encoded and stored!')
+        elif menu_option == '2':
+            print(f'The encoded password is {encoded_password}, and the original password is {password}.')
+        elif menu_option == '3':
+            quit_program = True
+
+
 if __name__ == '__main__':
-    print(encoder("12345555"))
-    print(decoder(encoder('12345555')
+    main()
